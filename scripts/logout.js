a66111c9743e687fc4105620027f7cfe31d16995
Ryan Forsten <ryanforsten@gmail.com>
2019-09-17 10:10:07 -0400

    A few more touches to Auth0 setup to display user name and picture #3

--- scripts/logout.js
+++ scripts/logout.js
@@ -5,6 +5,13 @@ const authProcess = remote.require('./scripts/authprocess');
 
 const webContents = remote.getCurrentWebContents();
 
+webContents.on('dom-ready', () => {
+    const profile = authService.getProfile();
+    document.getElementById('picture').src = profile.picture;
+    document.getElementById('name').innerText = profile.name;
+    //document.getElementById('success').innerText = 'You successfully used OpenID Connect and OAuth 2.0 to authenticate.';
+});
+
 document.getElementById('logout').onclick = async () => {
     await authProcess.createLogoutWindow();
     remote.getCurrentWindow().close();
