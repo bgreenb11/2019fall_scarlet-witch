855de81f711da017340472d2fa84fbb90bd79a71
Jacob-Duffy <jacobryanduffy98@gmail.com>
2019-09-30 12:04:44 -0400

    Helper can receive a premade task to send to api. Fix #19

--- scripts/HueAPI/HueHelper.js
+++ scripts/HueAPI/HueHelper.js
@@ -2,6 +2,10 @@ const config = require('../../config.json');
 const hue = require('node-hue-api').v3;
 const discovery = hue.discovery;
 const hueApi = hue.api;
+const lightState = hue.lightStates.LightState;
+const sceneState = hue.Scene;
+const sceneLightState = hue.lightStates.SceneLightState;
+const groupState = hue.lightStates.GroupLightState;
 
 const app_name = 'node-hue-api';
 const device_name = 'scarlet-witch';
@@ -29,6 +33,7 @@ async function createUser() {
     try {
         user = await temp_api.users.createUser(app_name, device_name);
         let api = await hueApi.create(ip, user);
+        config['users'].push(user);
     } catch(err) {
         handleError(err);
     }
@@ -45,17 +50,65 @@ async function loadUser(username) {
 function handleError(err) {
     if (err.getHueErrorType() === 101) console.error("Press Link button on Hue Bridge.");
     else if (err.getHueErrorType() === 3) console.error("Scene could not be found.");
-    else console.error(`Unknown Error: ${err.message}`);
+    else console.error(err.message);
 }
 
 async function connect() {
     console.log('Connecting');
-    if (config.users.length === 0) createUser();
-    else loadUser(config.users[0]);
+    var api;
+    if (config.users.length === 0) api = createUser();
+    else api = loadUser(config.users[0]);
+    return api;
 }
 
 function session() {
     return user_session;
 }
 
+function formatTask(task) {
+    console.log('Formatting task.');
+    var state;
+    if (task['task_type'] === "LIGHT") state = lightState;
+    else if (task['task_type'] === "SCENE") {
+        if (task['targets'].length) state = sceneState;
+        else state = sceneLightState;
+    }
+    else if (task['task_type'] === "GROUP") state = groupState;
+    else throw new Error('Invalid task type given.');
+    return state;
+}
+
+async function sendState(task) {
+    var state = formatTask(task);
+    var response;
+
+    console.log('Sending task.');
+    if (task['task_type'] === "LIGHT") {                                            //  Specified lights
+        response = task['targets'].map(id => {
+            session().scenes.setLightState(task['scene_name'], id, state);
+        });
+    } else if (task['task_type'] === "SCENE") {
+        if (task['targets'].length) {                                               //  Specific lights in scene
+            repsonse = task['targets'].map(id => {
+                session().scenes.updateLightState(task['scene_name'], id, state);
+            });
+        } else {      
+            response = task['scenes'].map(scene_id => {                             //  All scenes
+                session().scenes.update(scene_id, state);
+            })
+        }
+    } else if (task['task_type'] === "GROUP") {                                     //  All groups specified
+        response = task['groups'].map(group_id => {
+            session().groups.setGroupState(group_id, state);
+        });
+    }
+
+    return response;
+}
+
 const user_session = connect();
+
+module.exports = {
+    session,
+    sendState
+}
