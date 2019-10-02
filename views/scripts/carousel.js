f0d3a41663cd285cbdab9d753ec278d0bd7ca582
Benjamin Greenberg <bgreenb3@vols.utk.edu>
2019-10-01 23:39:12 -0400

    Added 2nd carousel & fixed bootstrap bug fixes #18

--- views/scripts/carousel.js
+++ views/scripts/carousel.js
@@ -1,24 +1,29 @@
 // Code adapted from https://www.codeply.com/go/zjZsn1ly29
 
-$('#recipeCarousel').carousel({
+const $ = require('jquery');
+
+$('#deviceCarousel').carousel({
 	interval: 10000
-  })
-  
-  $('.carousel .carousel-item').each(function(){
-	  var minPerSlide = 4;
-	  var next = $(this).next();
-	  if (!next.length) {
-	  next = $(this).siblings(':first');
-	  }
-	  next.children(':first-child').clone().appendTo($(this));
-	  
-	  for (let i=0;i<minPerSlide;i++) {
-		  next=next.next();
-		  if (!next.length) {
-			  next = $(this).siblings(':first');
-		  }
-		  
-		  next.children(':first-child').clone().appendTo($(this));
-	  }
+})
+
+$('#groupCarousel').carousel({
+	interval: 10000
+})
+
+$('.carousel .carousel-item').each(function () {
+	var minPerSlide = 4;
+	var next = $(this).next();
+	if (!next.length) {
+		next = $(this).siblings(':first');
+	}
+	next.children(':first-child').clone().appendTo($(this));
+
+	for (let i = 0; i < minPerSlide; i++) {
+		next = next.next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+
+		next.children(':first-child').clone().appendTo($(this));
+	}
 });
-  
