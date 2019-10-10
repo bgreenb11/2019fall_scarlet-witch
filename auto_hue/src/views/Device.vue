eecb292704c2a788fe62e185f984cc9ff32ade0e
daniel-troutman <dtroutm1@vols.utk.edu>
2019-10-09 22:54:10 -0400

    Added bulb image to device page #32

--- auto_hue/src/views/Device.vue
+++ auto_hue/src/views/Device.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="device">
+      <v-img
+      src="../assets/clear_bulb.png"
+      aspect-ratio="1"
+      class="yellow"
+    ></v-img>
       <v-color-picker></v-color-picker>
       <v-slider></v-slider>
   </div>
