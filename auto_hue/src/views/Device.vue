65fc320b712ea8c57e1cac0932ecb77b24c0c19e
daniel-troutman <dtroutm1@vols.utk.edu>
2019-10-13 22:55:13 -0400

    Added color updates to bulb image #32

--- auto_hue/src/views/Device.vue
+++ auto_hue/src/views/Device.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="device">
-      <v-img
-      src="../assets/clear_bulb.png"
-      aspect-ratio="1"
-      class="yellow"
-    ></v-img>
-      <v-color-picker></v-color-picker>
-      <v-slider></v-slider>
-      <v-switch></v-switch>
+    <v-row>
+      <v-col>
+        <img id="bulb" src="../assets/clear_bulb.png" width="300" v-bind:style="{backgroundColor: color}">
+      </v-col>
+      <v-col>
+        <v-color-picker
+          v-model="color"
+        ></v-color-picker>
+        <v-slider></v-slider>
+        <v-switch></v-switch>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 
 export default {
+  data: () => ({
+      color: '#000000'
+  }),
+
   name: 'Device',
   props: {
   },
