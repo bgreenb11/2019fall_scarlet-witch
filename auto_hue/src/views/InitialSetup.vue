f46d4cebe0825208557f7efc8615886e89cc2f99
Jacob-Duffy <jacobryanduffy98@gmail.com>
2019-10-22 00:38:12 -0400

    Cleaned look for setup #42

--- auto_hue/src/views/InitialSetup.vue
+++ auto_hue/src/views/InitialSetup.vue
@@ -1,57 +1,120 @@
 <template>
-  <v-card max-width="500" min-height="300" class="mx-auto my-8 inline-block">
+  <v-card width="350" height="auto" min-height="300" class="mx-auto my-8 inline-block">
     <div v-if="index === 0">
       <v-container>
-        <v-col cols="auto">
-          <v-row justify="center" align="center">
-            <v-card-title class="headline">Welcome!</v-card-title>
-          </v-row>
-          <v-row justify="center" align="center">
-            <v-card-text class="text-center">{{this.pages[this.index]}}</v-card-text>
-          </v-row>
-          <v-row>
-            <v-card-actions>
-              <v-btn v-on:click="navigate">Next</v-btn>
-            </v-card-actions>
-          </v-row>
-        </v-col>
+        <v-row no-gutters justify="space-between">
+          <v-col align="center">
+            <p class="display-1 text-center text-uppercase">Welcome</p>
+          </v-col>
+        </v-row>
+        <v-row no-gutters justify="space-between">
+          <v-col align="center">
+            <p class="subtitle-2 text-center text-uppercase">{{this.pages[this.index]}}</p>
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col align="center">
+            <v-img width="300" height="300" src="../assets/HueBridgeSetup/huebridge.svg" />
+          </v-col>
+        </v-row>
+        <v-row no-gutters justify="space-around">
+          <v-col align="center">
+            <v-btn block text v-on:click="navigate">Next</v-btn>
+          </v-col>
+        </v-row>
       </v-container>
     </div>
     <div v-if="index === 1">
-      <v-list two-line class="overflow-y-auto" max-height="300">
-        <v-list-item v-for="bridge in bridges" :key="bridge.id" v-on:click="chooseBridge(bridge)">
-          <v-list-item-content>
-            <v-list-item-title v-text="bridge.id" />
-            <v-list-item-subtitle v-text="bridge.internalipaddress" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+      <v-container>
+        <v-row no-gutters justify="space-between">
+          <v-col align="center">
+            <p class="display-1 text-center text-uppercase">Setup</p>
+          </v-col>
+        </v-row>
+        <v-row no-gutters justify="space-between">
+          <v-col align="center">
+            <p class="subtitle-2 text-center text-uppercase">{{this.pages[this.index]}}</p>
+          </v-col>
+        </v-row>
+        <v-row no-gutters justify="space-between">
+          <v-col align="center">
+            <v-list class="overflow-y-auto" max-height="300">
+              <v-list-item
+                v-for="bridge in bridges"
+                :key="bridge.id"
+                v-on:click="chooseBridge(bridge)"
+              >
+                <v-list-item-content>
+                  <v-list-item-title v-text="bridge.id" />
+                  <v-list-item-subtitle v-text="bridge.internalipaddress" />
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-col>
+        </v-row>
+      </v-container>
     </div>
     <div v-if="index === 2">
       <v-container>
-        <v-col cols="auto">
-          <v-row justify="center" align="center">
-            <v-card-title class="headline">Setup</v-card-title>
-          </v-row>
-          <v-row justify="center" align="center">
-            <v-card-text class="text-center">{{this.pages[this.index]}}</v-card-text>
-          </v-row>
-          <v-row justify="center" align="center" height="auto" width="auto">
-            <v-img contain width="200" height="200" src="../assets/huebridgediagram.svg" />
-          </v-row>
-        </v-col>
+        <v-row no-gutters justify="space-between">
+          <v-col align="center">
+            <p class="display-1 text-center text-uppercase">Setup</p>
+          </v-col>
+        </v-row>
+        <v-row no-gutters justify="space-between">
+          <v-col align="center">
+            <p class="subtitle-2 text-center text-uppercase">{{this.pages[this.index]}}</p>
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col align="center">
+            <v-img width="300" height="300" src="../assets/HueBridgeSetup/huebridgepress.svg" />
+          </v-col>
+        </v-row>
+        <v-row no-gutters justify="space-around">
+          <v-col align="center">
+            <v-btn :disabled="loading" :loading="loading" block text v-on:click="navigate">Next</v-btn>
+          </v-col>
+        </v-row>
       </v-container>
     </div>
-    <div v-if="index === 3">
+    <div v-if="index === 3 || index === 4">
       <v-container>
-        <v-col cols="auto">
-          <v-row justify="center" align="center">
-            <v-card-title class="headline">Setup</v-card-title>
-          </v-row>
-          <v-row justify="center" align="center">
-            <v-card-text class="text-center">{{this.pages[this.index]}}</v-card-text>
-          </v-row>
-        </v-col>
+        <v-row no-gutters justify="space-between">
+          <v-col align="center">
+            <p class="display-1 text-center text-uppercase">Setup</p>
+          </v-col>
+        </v-row>
+        <v-row no-gutters justify="space-between">
+          <v-col align="center">
+            <p class="subtitle-2 text-center text-uppercase">{{this.pages[this.index]}}</p>
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col align="center">
+            <v-img v-if="index === 3" width="300" height="300" src="../assets/HueBridgeSetup/huebridgesuccess.svg" />
+            <v-img v-if="index === 4" width="300" height="300" src="../assets/HueBridgeSetup/huebridgefail.svg" />
+          </v-col>
+        </v-row>
+        <v-row no-gutters justify="space-around" v-if="index === 3">
+          <v-col align="center">
+            <v-btn block text v-on:click="navigate">Proceed</v-btn>
+          </v-col>
+        </v-row>
+        <v-row no-gutters justify="space-between" v-if="index === 4">
+          <v-col align="center">
+            <v-row>
+            <v-btn
+              :disabled="loading"
+              :loading="loading"
+              block
+              text
+              v-on:click="navigate"
+            >Try Again</v-btn>
+            <v-btn block text v-on:click="navigate">Quit</v-btn>
+            </v-row>
+          </v-col>
+        </v-row>
       </v-container>
     </div>
   </v-card>
@@ -67,10 +130,11 @@ export default {
       bridges: [],
       bridge: null,
       hash_username: null,
+      loading: false,
       pages: [
         "Please locate your bridge.",
-        "Please select your bridge:",
-        "Press and hold the center button on your bridge.",
+        "Please select your bridge below.",
+        "Press and hold the center button on your bridge. This process should take about 10 seconds.",
         "Success!",
         "Sorry. We couldn't find your bridge. Please make sure to press and hold the center button when trying to link. Would you like to try again?"
       ]
@@ -78,19 +142,22 @@ export default {
   },
   methods: {
     navigate: function() {
+      console.log(this.index);
       switch (this.index) {
         case 0:
           this.index += 1;
           this.findAllBridges();
           break;
         case 2:
-          console.log("Done");
+          this.loading = true;
+          this.start_linkBridge();
           break;
         case 3:
-          this.linkBridge();
+          console.log("Done");
           break;
-        default:
-          console.log(this.index);
+        case 4:
+          this.loading = true;
+          this.start_linkBridge();
           break;
       }
     },
@@ -100,31 +167,38 @@ export default {
       });
     },
     chooseBridge: function(b) {
+      console.log(this.index);
       this.bridge = b;
       this.index += 1;
-      console.log(this.index);
-      this.start_linkBridge();
     },
     start_linkBridge: function() {
+      console.log(this.index);
       self = this;
       setTimeout(function() {
         self.linkBridge();
       }, 10000);
     },
     linkBridge: function() {
-      console.log("linking")
       var json = {
         devicetype: "auto_hue#desktop user"
       };
       axios
         .post(`http://${this.bridge.internalipaddress}/api/`, json)
         .then(response => {
-          console.log(JSON.stringify(response));
-          this.hash_username = response.data[0].success.username;
-          console.log(this.hash_username);
-          this.index += 1;
+          this.loading = false;
+          console.log(this.loading);
+          try {
+            this.hash_username = response.data[0].success.username;
+            this.index = this.pages.length - 2;
+          } catch (error) {
+            console.error(error);
+            this.index = this.pages.length - 1;
+          }
         });
     }
+  },
+  watch: {
+    loader() {}
   }
 };
 </script>
