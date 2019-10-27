65331ada2a983578b4de6771118b6fc794bc3f49
Daniel Troutman <dtroutm1@vols.utk.edu>
2019-10-27 00:21:01 -0400

    Initial page with control of groups #53

--- auto_hue/src/router.js
+++ auto_hue/src/router.js
@@ -31,6 +31,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import( /* webpackChunkName: "device" */ './views/Device.vue')
+    },
+    {
+      path: '/group',
+      name: 'group',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import( /* webpackChunkName: "group" */ './views/Group.vue')
     }
   ]
 })
