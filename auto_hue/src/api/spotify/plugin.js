362440bab80f93585ef34e500752a7f9958d75ee
Jacob-Duffy <jacobryanduffy98@gmail.com>
2019-11-07 11:27:34 -0500

    Setting groundwork for spotify player. #60

---
+++ auto_hue/src/api/spotify/plugin.js
@@ -0,0 +1,37 @@
+import spotify from './index';
+
+let isFetchingToken = false;
+
+const plugin = store => {
+  spotify.interceptors.spotify.use(function (config) {
+    if (store.getters['auth/getAccessToken']) {
+      config.headers.common['Authorization'] = `Bearer ${store.getters['auth/getAccessToken']}`;
+    }
+    return config;
+  }, null);
+
+  spotify.interceptors.response.use(null, (error) => {
+    const {status, data} = error.response;
+
+    if (store.getters['auth/getAccessToken'] && status === 401 && !isFetchingToken) {
+      isFetchingToken = true;
+      store.dispatch('auth/refreshToken').then(()=>{
+        isFetchingToken = false;
+        location.reload();
+      });
+    } else if (status === 404) {
+      throw error.response;
+    } else if (status === 403) {
+      if( data.error.reason === 'PREMIUM_REQUIRED') {
+        store.dispatch('notification/add', {
+          type: 'warning',
+          message: 'You need to have premium account.',
+        })
+      }
+    } else {
+      store.dispatch('auth/login');
+    }
+  });
+};
+
+export default plugin;
--- auto_hue/src/store/index.js
+++ auto_hue/src/store/index.js
@@ -2,6 +2,7 @@ import Vuex from 'vuex';
 import Vue from 'vue';
 import devices from './modules/devices';
 import bridges from './modules/bridges';
+import spotifyAuth from './modules/spotifyAuth'
 
 // Load Vuex
 Vue.use(Vuex);
@@ -11,5 +12,6 @@ export default new Vuex.Store({
     modules: {
         devices,
         bridges,
+        spotifyAuth
     }
 })
