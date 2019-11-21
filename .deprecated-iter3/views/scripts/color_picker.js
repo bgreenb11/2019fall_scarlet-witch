888cf15058f658ce9c02af3f4a9f7c2e93980fdf
Benjamin Greenberg <bgreenb3@vols.utk.edu>
2019-11-21 00:26:21 -0500

    Cleaned up directories

--- .deprecated-iter3/views/scripts/color_picker.js
+++
@@ -1,7 +0,0 @@
-var colorPicker = new iro.ColorPicker(".colorPicker");
-
-var bulb = document.getElementById("bulb");
-
-colorPicker.on(["color:init", "color:change"], function(color){
-    bulb.style.backgroundColor = color.hexString;
-  });
